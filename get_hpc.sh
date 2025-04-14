e4dfa1617c926f0f6e8d91894a6475000e4895ab
kashish srivastava <kash.pegasus@gmail.com>
2025-04-14 11:50:06 +0530

    bngd

--- get_hpc.sh
+++ get_hpc.sh
@@ -1,1 +1,2 @@
-rsync -aWvr --progress   aib242289@hpc.iitd.ac.in:~/AIL862/Assignment5/cityscapes/ Assignment5/cityscapes/
+#rsync -aWvr --progress   aib242289@hpc.iitd.ac.in:~/AIL862/Assignment5/cityscapes/gt.zip Assignment5/cityscapes/gt.zip
+scp -r  aib242289@hpc.iitd.ac.in:~/AIL862/Assignment5/cityscapes/gt.zip ./Assignment5/cityscapes/
